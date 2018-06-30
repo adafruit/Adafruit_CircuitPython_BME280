8b501a370b1a2a7f32e33b853d9af1eeb44af9db
ladyada <limor@ladyada.net>
2018-06-29 21:32:26 -0400

    prepare to pypify

--- docs/conf.py
+++ docs/conf.py
@@ -16,8 +16,6 @@ extensions = [
 ]
 
 # API docs fix
-autodoc_mock_imports = ['micropython']
-
 intersphinx_mapping = {'python': ('https://docs.python.org/3.4', None),'BusDevice': ('https://circuitpython.readthedocs.io/projects/busdevice/en/latest/', None),'CircuitPython': ('https://circuitpython.readthedocs.io/en/latest/', None)}
 
 # Add any paths that contain templates here, relative to this directory.
