6fb01e73c455e56fbc88e514f332db2d66cc897d
dherrada <dylan.herrada@gmail.com>
2020-03-16 15:16:30 -0400

    Ran black, updated to pylint 2.x

--- examples/bme280_normal_mode.py
+++ examples/bme280_normal_mode.py
@@ -14,9 +14,9 @@ i2c = busio.I2C(board.SCL, board.SDA)
 bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
 
 # OR create library object using our Bus SPI port
-#spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
-#bme_cs = digitalio.DigitalInOut(board.D10)
-#bme280 = adafruit_bme280.Adafruit_BME280_SPI(spi, bme_cs)
+# spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
+# bme_cs = digitalio.DigitalInOut(board.D10)
+# bme280 = adafruit_bme280.Adafruit_BME280_SPI(spi, bme_cs)
 
 # Change this to match the location's pressure (hPa) at sea level
 bme280.sea_level_pressure = 1013.25
