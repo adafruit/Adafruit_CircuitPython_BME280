a1be6526d1beacf7525ad7736968e86249ff33a0
anecdata <16617689+anecdata@users.noreply.github.com>
2020-11-24 21:20:30 -0600

    update readme and examples

--- examples/bme280_normal_mode.py
+++ examples/bme280_normal_mode.py
@@ -31,7 +31,7 @@ time.sleep(1)
 
 while True:
     print("\nTemperature: %0.1f C" % bme280.temperature)
-    print("Humidity: %0.1f %%" % bme280.humidity)
+    print("Humidity: %0.1f %%" % bme280.relative_humidity)
     print("Pressure: %0.1f hPa" % bme280.pressure)
     print("Altitude = %0.2f meters" % bme280.altitude)
     time.sleep(2)
--- examples/bme280_simpletest.py
+++ examples/bme280_simpletest.py
@@ -18,7 +18,7 @@ bme280.sea_level_pressure = 1013.25
 
 while True:
     print("\nTemperature: %0.1f C" % bme280.temperature)
-    print("Humidity: %0.1f %%" % bme280.humidity)
+    print("Humidity: %0.1f %%" % bme280.relative_humidity)
     print("Pressure: %0.1f hPa" % bme280.pressure)
     print("Altitude = %0.2f meters" % bme280.altitude)
     time.sleep(2)
