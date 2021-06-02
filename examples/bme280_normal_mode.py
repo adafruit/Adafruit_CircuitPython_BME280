3f6f12591eabf221b6bf88e3648f9f807988939d
jposada202020 <jquintana202020@gmail.com>
2021-06-02 14:10:38 -0400

    improving_importing_description

--- examples/bme280_normal_mode.py
+++ examples/bme280_normal_mode.py
@@ -12,23 +12,23 @@ import adafruit_bme280.advanced as adafruit_bme280
 
 # Create sensor object, using the board's default I2C bus.
 i2c = board.I2C()  # uses board.SCL and board.SDA
-bme280 = advanced.Adafruit_BME280_I2C(i2c)
+bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
 
 # OR create sensor object, using the board's default SPI bus.
 # SPI setup
 # from digitalio import DigitalInOut
 # spi = board.SPI()
 # bme_cs = digitalio.DigitalInOut(board.D10)
-# bme280 = advanced.Adafruit_BME280_SPI(spi, bme_cs)
+# bme280 = adafruit_bme280.Adafruit_BME280_SPI(spi, bme_cs)
 
 # Change this to match the location's pressure (hPa) at sea level
 bme280.sea_level_pressure = 1013.25
-bme280.mode = advanced.MODE_NORMAL
-bme280.standby_period = advanced.STANDBY_TC_500
-bme280.iir_filter = advanced.IIR_FILTER_X16
-bme280.overscan_pressure = advanced.OVERSCAN_X16
-bme280.overscan_humidity = advanced.OVERSCAN_X1
-bme280.overscan_temperature = advanced.OVERSCAN_X2
+bme280.mode = adafruit_bme280.MODE_NORMAL
+bme280.standby_period = adafruit_bme280.STANDBY_TC_500
+bme280.iir_filter = adafruit_bme280.IIR_FILTER_X16
+bme280.overscan_pressure = adafruit_bme280.OVERSCAN_X16
+bme280.overscan_humidity = adafruit_bme280.OVERSCAN_X1
+bme280.overscan_temperature = adafruit_bme280.OVERSCAN_X2
 # The sensor will need a moment to gather initial readings
 time.sleep(1)
 
