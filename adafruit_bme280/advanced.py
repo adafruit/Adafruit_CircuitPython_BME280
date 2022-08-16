5b66e5b22aaf318ba197f4915ea81f38ff5be613
Alec Delaney <tekktik@gmail.com>
2022-08-16 18:09:12 -0400

    Update version string

--- adafruit_bme280/advanced.py
+++ adafruit_bme280/advanced.py
@@ -39,7 +39,7 @@ try:
 except ImportError:
     pass
 
-__version__ = "0.0.0-auto.0"
+__version__ = "0.0.0+auto.0"
 __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_BME280.git"
 
 #    I2C ADDRESS/BITS/SETTINGS
